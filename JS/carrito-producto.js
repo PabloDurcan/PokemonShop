38524e754fca624584932509e8232f9e7a30da50
pablo <pablodurcan@gmail.com>
2021-05-17 20:55:59 +0200

    funcionalidad carrito productos individual

--- JS/carrito-producto.js
+++ JS/carrito-producto.js
@@ -5,6 +5,14 @@ function sacarSubtotal() {
     let subtotal = contador * precioProducto;
     subtotal2 = subtotal.toFixed(2);  
     document.getElementById("precio-subtotal").innerHTML = subtotal2 + " €";
+    document.getElementById("precio-subtotal-cesta").innerHTML = subtotal2 + " €";
+
+}
+
+function sacarTeclado(){
+    contador = document.getElementById("input-cantidad").value;
+
+    sacarSubtotal();
 }
 
 document.getElementById("btn-mas").addEventListener("click",function(){
@@ -19,5 +27,22 @@ document.getElementById("btn-menos").addEventListener("click",function(){
         document.getElementById("input-cantidad").value = contador;
         sacarSubtotal();   
     }
+});
+
+document.getElementById("add-carrito").addEventListener("click",function(){
+    document.getElementById("precio-cesta").innerHTML = "1";
+    document.getElementById("cantidad-cesta").innerHTML = contador;
+
+    document.getElementById("cuerpo-cesta").style.display = "";
+    document.getElementById("cuerpo-cesta-vacio").style.display = "none";
+
+});
+
+document.getElementById("remove-carrito").addEventListener("click",function(){
+    document.getElementById("precio-cesta").innerHTML = "0";
+
+    document.getElementById("cuerpo-cesta").style.display = "none";
+    document.getElementById("cuerpo-cesta-vacio").style.display = "";
+
 });
 
